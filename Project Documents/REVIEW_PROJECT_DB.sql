2d10de278af5f04fe61621d3ad33efeafc27163a
JZachary07 <JZachary8696@gmail.com>
2018-10-22 17:28:54 -0400

    Add files via upload
    Moving some files to appropriate directory

---
+++ Project Documents/REVIEW_PROJECT_DB.sql
@@ -0,0 +1,21 @@
+CREATE SCHEMA REVIEW_PROJECT_DB;
+
+CREATE TABLE REG_USER (
+	account_ID CHAR(9) PRIMARY KEY NOT NULL,
+	username VARCHAR(32) UNIQUE NOT NULL,
+	display_name VARCHAR(64),
+	password_hash CHAR(128) NOT NULL,
+	password_salt CHAR(32) NOT NULL,
+	email VARCHAR(64) UNIQUE NOT NULL,
+	is_banned TINYINT(1) DEFAULT 0);
+
+CREATE TABLE REVIEW (
+	review_ID CHAR(9) PRIMARY KEY NOT NULL,
+	posting_account_ID CHAR(9),
+	display_email CHAR(64),
+	disp_name CHAR(32),
+	post_date_time TIMESTAMP(),
+	posting_IP VARCHAR(26) DEFAULT "0.0.0.0",
+	review_text TEXT(2000) NOT NULL,
+	is_valid TINYINT(1) DEFAULT 1 NOT NULL,
+	FOREIGN KEY(posting_account_ID) REFERENCES REG_USER(account_ID) ON DELETE CASCADE ON UPDATE CASCADE);
