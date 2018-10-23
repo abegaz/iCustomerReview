4cc20ca397409b56ad1f306efe7fdf607e5a125c
spgiff2651 <42653539+spgiff2651@users.noreply.github.com>
2018-10-23 12:15:42 -0400

    Add files via upload
    Updated database

--- Project Documents/fakeapples.sql
+++ Project Documents/fakeapples.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 18, 2018 at 09:04 PM
+-- Generation Time: Oct 23, 2018 at 06:15 PM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.10
 
@@ -29,13 +29,24 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `account` (
-  `account_ID` varchar(20) COLLATE utf8_bin NOT NULL,
+  `account_ID` int(11) NOT NULL,
   `username` varchar(15) COLLATE utf8_bin NOT NULL,
-  `password_hash` varchar(32) COLLATE utf8_bin NOT NULL,
+  `password_hash` varchar(100) COLLATE utf8_bin NOT NULL,
   `password_salt` char(32) COLLATE utf8_bin NOT NULL,
   `email` varchar(30) COLLATE utf8_bin NOT NULL,
-  `isBanned` tinyint(1) NOT NULL DEFAULT '0'
+  `firstName` varchar(15) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `lastName` varchar(15) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `isBanned` tinyint(1) NOT NULL DEFAULT '0',
+  `displayName` varchar(25) COLLATE utf8_bin NOT NULL DEFAULT ''
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `account`
+--
+
+INSERT INTO `account` (`account_ID`, `username`, `password_hash`, `password_salt`, `email`, `firstName`, `lastName`, `isBanned`, `displayName`) VALUES
+(1, 'pouchbunny', '2828d2205a8de6b982fbb3f18b26eaecfc0e8558349f3eb4c9daec1cd6755b36', '[C@2d0756da', 'pouchbunny@timbrfawks.com', '', '', 0, 'timbrfawks'),
+(2, 'spgiff2651', '026492a56caaac48e2139204a4db88fc7278264efc491ec2627e2ee8d7c33c0a', '[C@5af76ef4', 'stephen@ung.edu', '', '', 0, 'stephen');
 
 -- --------------------------------------------------------
 
@@ -108,6 +119,16 @@ ALTER TABLE `purchase`
 --
 ALTER TABLE `review`
   ADD PRIMARY KEY (`review_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `account`
+--
+ALTER TABLE `account`
+  MODIFY `account_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
