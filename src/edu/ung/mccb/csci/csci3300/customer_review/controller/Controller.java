372cfd847a9bbc5977abf736c17153bd3c26aaee
Brisaac Johnson <42587507+brisaacjohnson@users.noreply.github.com>
2018-11-01 10:11:08 -0400

    :/ mapping out some logic here

--- src/edu/ung/mccb/csci/csci3300/customer_review/controller/Controller.java
+++ src/edu/ung/mccb/csci/csci3300/customer_review/controller/Controller.java
@@ -18,9 +18,36 @@ public class Controller {
     TextField reviewText;
     Slider rating;
     private String IP;
+    
+///Brisaac is mapping out some logic here nothing here is finite. I know some of these methods are wrong.
+   public void saveToDB (ActionEvent actionEvent) throws Exception {
+       //Model model = new Model();
+       //String userPassword =  password.getText();
 
-   public void saveToDB (ActionEvent actionEvent) {
-        // TODO: method body
+       boolean isValid= validatePassword(userPassword, confirmUserPassword);
+       /*
+       if (verifyCaptcha) {
+
+            //Data will be saved into the DB
+           int * = *.saveUserIntoDatabase(reviewField.getText(),*more get methods needed);
+           System.out.println("Users review was saved into the database");
+
+          //New stage is generated, shouls this stage go to the display review for the Products fxml
+
+           Stage primaryStage= new Stage();
+           Parent root = FXMLLoader.load(getClass().getResource("*"));
+           primaryStage.setTitle("*");
+           primaryStage.setScene(new Scene(root, 425, 400));
+           primaryStage.show();
+
+       }
+
+       else
+       {
+         //message or another window that displays the an error message rejecting the review that submitted
+       }
+        */
+
    }
 
    public void verifyCaptcha () {
